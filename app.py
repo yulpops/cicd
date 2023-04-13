4450c99e4cf9e5292a5c882336ece6824e18e2bd
yulpops <129699350+yulpops@users.noreply.github.com>
2023-04-13 13:36:40 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request, jsonify,render_template
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('hello.html')
+
+
+
+
+
+app.run(port=8000)
+
+if __name__ == '__main__':
+    main()
